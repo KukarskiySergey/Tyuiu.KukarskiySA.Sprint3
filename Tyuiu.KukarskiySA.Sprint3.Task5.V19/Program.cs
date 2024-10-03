d66c854005e58571205aafc885c605352ce6dd7d
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-10-04 01:29:16 +0500

    Task5

---
+++ Tyuiu.KukarskiySA.Sprint3.Task5.V19/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KukarskiySA.Sprint3.Task5.V19.Lib;
+
+DataService dataService = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Кукарский С.А. | ИИПб-24-1";
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* Спринт #3                                                            *");
+Console.WriteLine("* Тема: Операторы составного присваивания                              *");
+Console.WriteLine("* Задание #5                                                           *");
+Console.WriteLine("* Вариант #19                                                          *");
+Console.WriteLine("* Выполнил Кукарский Сергей Андреевич | ИИПб-24-1                      *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                             *");
+Console.WriteLine("* Отсутствует                                                          *");
+Console.WriteLine("*                                                                      *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+Console.WriteLine("* Формула                                                              *");
+Console.WriteLine("*                                                                      *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+
+int x = 2;
+double result = dataService.GetSumSumSeries(x, 1, 1, 3, 6);
+
+Console.WriteLine($"Результат суммы: {result}");
+Console.WriteLine("************************************************************************");
+Console.ReadLine();
